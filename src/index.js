e40f1a8015956edeeb2c6d5e8a156d6a234273d2
최지인 <genie1019002@gmail.com>
2025-09-26 14:12:07 +0900

    ss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import InfoForm from './jsx/signup/InfoForm2';
 
 // ✅ 서비스워커 등록 (푸시 알림/오프라인 캐시용)
 if ('serviceWorker' in navigator) {
@@ -23,7 +22,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <InfoForm/>
   </React.StrictMode>
 );
 
